0ed9c512ded2d4ee18a7e53f6ab324383ba2c49b
shakerqarizada <afghanshaker21@gmail.com>
2025-12-25 17:40:00 +0430

    Style update

--- src/script.js
+++ src/script.js
@@ -1,6 +1,11 @@
 let menu = document.querySelector("#menu-toggle");
-menu.addEventListener("click",()=>{
-   document.querySelector("#nav-menu").classList.toggle("hidden");
+menu.addEventListener("click", () => {
+  document.querySelector("#nav-menu").classList.remove("hidden");
 });
 
-
+let closeButtons = document.querySelectorAll(".menu-close");
+closeButtons.forEach((btn) => {
+  btn.addEventListener("click", () =>
+    document.querySelector("#nav-menu").classList.add("hidden")
+  );
+});
